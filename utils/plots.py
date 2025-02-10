c73e26de62e0239f5bc8aade8273ca39f646f9e3
lalondma <marc.lalonde@crim.ca>
2025-02-10 16:18:14 -0500

    Update plots.py
    https://github.com/ultralytics/yolov5/issues/11838
    
    Signed-off-by: lalondma <marc.lalonde@crim.ca>

--- utils/plots.py
+++ utils/plots.py
@@ -83,7 +83,10 @@ class Annotator:
         if self.pil or not is_ascii(label):
             self.draw.rectangle(box, width=self.lw, outline=color)  # box
             if label:
-                w, h = self.font.getsize(label)  # text width, height
+                #w, h = self.font.getsize(label)  # text width, height
+                left, top, right, bottom = self.font.getbbox(label)
+                w = right - left
+                h = bottom - top
                 outside = box[1] - h >= 0  # label fits outside box
                 self.draw.rectangle([box[0],
                                      box[1] - h if outside else box[1],
